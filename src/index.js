f1e5ec77a5e1ebf361b58a1b48c54b1a0390c78b
Vitali Kulsha <73823615+vitalikulsha@users.noreply.github.com>
2022-06-22 20:33:24 +0300

    feat: add code in function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function toReadable (number) {
-  
+    if(number < 0 || number >= 1000){
+    return 'The number '+ number +' is out of bounds!';
+  }
+  const numsFromZeroToNine = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const numsFromTenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const numsTens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const count = String(number).length;
+  let res = 0, 
+      nums = '';      
+  for(let i = count; i > 0; i--){
+    switch(i){
+      case 3:
+        res = Math.trunc(number / 100);
+        nums += res + ' ';
+        number -= res * 100;
+        break;
+      case 2:
+        res = Math.trunc(number / 10);
+        if(res === 1){
+          nums += numsFromTenToNineteen[number - 10];
+          break;
+        } else {
+          nums += numsTens[res - 2] + ' ';
+        }
+        number -= res * 10;
+        break;
+      case 1:
+        nums += numsFromZeroToNine[number];
+        break;
+    } 
+  }      
+  return nums;
 }
